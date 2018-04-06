5075be28448c1243f649b6eacb2634e00ddda7f8
Eric Ludlow <esludlow@gmail.com>
2018-04-06 10:01:12 -0600

    adds podfile and realm and kf pods

---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '11.2'
+
+target 'FetchAndViewProfiles' do
+
+use_frameworks!
+
+pod 'RealmSwift', '~> 3.3.2'
+pod 'Kingfisher', '~> 4.7.0'
+
+end
---
+++ Pods/Target Support Files/Pods-FetchAndViewProfiles/Pods-FetchAndViewProfiles-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FetchAndViewProfiles : NSObject
+@end
+@implementation PodsDummy_Pods_FetchAndViewProfiles
+@end
